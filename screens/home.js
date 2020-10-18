3ef75e20a37529719c29c3c613a10276c736f3b6
fareedmabrouk <fareed72@uw.edu>
2020-10-18 01:50:38 -0700

    initial home page

--- screens/home.js
+++ screens/home.js
@@ -1,9 +1,12 @@
-import React from "react";
-import { SafeAreaView, Text, StyleSheet, StatusBar } from "react-native";
+import React, { useState } from "react";
+import { View, SafeAreaView, Text, StyleSheet, StatusBar, TextInput, Button, Picker } from "react-native";
 import { theme } from "../styles";
 import { LinearGradient } from "expo-linear-gradient";
+import { Picker } from '@react-native-community/picker';
 
 const Home = () => {
+  const [value, onChangeText] = useState();
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -17,6 +20,31 @@ const Home = () => {
           }}
         >
           <Text style={styles.text}>Enter An Address</Text>
+          <TextInput
+            style={{ height: 100, color: 'white' }}
+            placeholder="Ex: Safeway 19th St."
+            onChangeText={text => onChangeText(text)}
+            value={value}
+          />
+          
+          <Picker
+          >
+
+          </Picker>
+          <View style={{flexDirection: 'row'}}>
+            <Button
+              onPress={console.log('test')}
+              title='Review Safety'
+              color='white'
+              accessibilityLabel='Review a business sanitation'
+            />
+            <Button
+              onPress={console.log('test')}
+              title='Rate Business'
+              color='white'
+              accessibilityLabel='Review a business sanitation'
+            />
+          </View>
         </LinearGradient>
       </SafeAreaView>
       <SafeAreaView style={{ flex: 0, backgroundColor: "#045DE9" }} />
